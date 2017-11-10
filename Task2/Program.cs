43c249d7350da0960770cc4ae727d6c6a268fddf
_DQ_ <_DQ_@NOTEBOOK>
2017-11-10 23:58:43 +0300

    Стартовый

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stroka;
+            stroka = System.Console.In.ReadLine();
+            System.Console.Out.WriteLine(stroka);
+        }
+    }
+}
